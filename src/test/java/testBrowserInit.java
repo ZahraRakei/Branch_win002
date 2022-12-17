64ab1263dc87931ef9445871da7f1b980da3121e
=ZahraRakei <=zrakei.qa@gmail.com>
2022-12-16 23:52:37 -0500

    MerryDeals added

--- src/test/java/testBrowserInit.java
+++ src/test/java/testBrowserInit.java
@@ -1,4 +1,6 @@
 import base.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,5 +12,14 @@ public class testBrowserInit extends Base {
         initBrowser("chrome");
         driver.get(Base.URL);
         Assert.assertEquals(driver.getTitle(), "Amazon.com. Spend less. Smile more.");
+    }
+
+    @Test
+    public void testDeals(){
+        initBrowser("chrome");
+        driver.get(Base.URL);
+        WebElement merryDeals = driver.findElement(By.linkText("Very Merry Deals"));
+        merryDeals.click();
+        Assert.assertEquals(driver.getTitle(), "Amazon.com - Today's Deals");
     }
 }
